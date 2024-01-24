5e59f1a6f80e460aea30133a4d0913f696c4c042
Vasster <vasilhspinhs@gmail.com>
2024-01-24 23:00:54 +0200

    Transition from Unity Script to WPF
    -The countdown timer works now but only with debugs.

---
+++ BoxingRoundIntervalWPF/BoxingRoundIntervalWFP/BoxingRoundIntervalManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingRoundIntervalWFP
+{
+    internal class BoxingRoundIntervalManager
+    {
+        private MainWindow mainWindow;
+
+        public BoxingRoundIntervalManager(MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+        }
+        public void Initialize()
+        {
+
+            Console.WriteLine("BoxingRoundIntervalManager initialized.");
+            mainWindow.BeginCountdown();
+        }
+    }
+}
---
+++ BoxingRoundIntervalWPF/BoxingRoundIntervalWFP/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace BoxingRoundIntervalWFP
+{
+    
+    public partial class MainWindow : Window
+    {
+        private BoxingRoundIntervalManager manager;
+        private DispatcherTimer countdownTimer;
+        private int countdownTime = 5;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            manager = new BoxingRoundIntervalManager(this);
+            manager.Initialize();
+            InitializeCountdownTimer();
+            BeginCountdown();
+        }
+
+        private void InitializeCountdownTimer()
+        {
+            countdownTimer = new DispatcherTimer();
+            countdownTimer.Interval = TimeSpan.FromSeconds(1);
+            countdownTimer.Tick += GetReadyCountdownTimer;
+        }
+
+        private void GetReadyCountdownTimer(object sender, EventArgs e)
+        {
+            if (countdownTimer != null) 
+            {
+                if (countdownTime > 0)
+                {
+                    countdownTime--;
+                    UpdateUI();
+                }
+                else
+                {
+                    countdownTimer.Stop();
+                }
+            }
+        }
+
+        private void UpdateUI()
+        {
+            int minutes = countdownTime / 60;
+            int seconds = countdownTime % 60;
+            Debug.WriteLine($"Time Remaining: {minutes:00}:{seconds:00}");
+            Debug.WriteLine("GET READY!");
+        }
+
+       public void BeginCountdown()
+        {
+            if (countdownTimer != null)
+            {
+                countdownTimer.Start();
+            }
+        }
+    }
+}
